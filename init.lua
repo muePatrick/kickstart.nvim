cad4697602442ed34c1529b06e5e543d336f9f3b
Patrick Müller <patrick@muepatrick.dev>
2023-03-19 12:38:33 +0100

    Add lsps
    - add lsps for python, typescript, eslint
    - leader+enter for code actions
    - leader+. to fix all eslint errors

--- init.lua
+++ init.lua
@@ -382,6 +382,7 @@ local on_attach = function(_, bufnr)
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader><enter>', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
@@ -416,9 +417,10 @@ end
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
+  pyright = {},
   -- rust_analyzer = {},
-  -- tsserver = {},
+  tsserver = {},
+  eslint = {},
 
   lua_ls = {
     Lua = {
@@ -427,6 +429,8 @@ local servers = {
     },
   },
 }
+
+vim.keymap.set('n', '<leader>.', ':EslintFixAll<CR>', { desc = 'Eslint: Fix All' })
 
 -- Setup neovim lua configuration
 require('neodev').setup()
