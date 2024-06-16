bfa269561d806a1efd3209cad090fc03d853e3bc
Patrick Müller <patrick@muepatrick.dev>
2024-06-16 20:15:20 +0200

    Fix Neotest

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -6,6 +6,7 @@ return {
       -- "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
       "nvim-neotest/neotest-go",
+      "nvim-neotest/nvim-nio",
     },
     config = function()
       require("neotest").setup({
@@ -20,8 +21,15 @@ return {
         },
       })
       vim.keymap.set('n', '<leader>t<enter>', require("neotest").run.run, { desc = '[T]est Run (nearest)' })
+      vim.keymap.set('n', '<leader>tt',
+        function()
+          require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        { desc = '[T]est Run [T]estfile' }
+      )
       vim.keymap.set('n', '<leader>tp', require("neotest").output_panel.toggle, { desc = '[T]est [P]anel' })
       vim.keymap.set('n', '<leader>ts', require("neotest").summary.toggle, { desc = '[T]est [S]ummary' })
+      vim.keymap.set('n', '<leader>to', require("neotest").output.open, { desc = '[T]est [O]utput' })
     end,
   }
 }
