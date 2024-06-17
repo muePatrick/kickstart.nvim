f9e5dd8ed03e88b7a0fcf11f403457095a0d0437
Patrick Müller <patrick@muepatrick.dev>
2024-06-17 12:10:11 +0200

    Add plugin to highlight undos and redos
    Whenever an undo or a redo is preformed briefly highlight the text that
    was changed.

---
+++ lua/custom/plugins/highlight-undo.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'tzachar/highlight-undo.nvim',
+    config = function()
+      require('highlight-undo').setup({
+        duration = 200,
+        undo = {
+          hlgroup = 'HighlightUndo',
+          mode = 'n',
+          lhs = 'u',
+          map = 'undo',
+          opts = {}
+        },
+        redo = {
+          hlgroup = 'HighlightRedo',
+          mode = 'n',
+          lhs = '<C-r>',
+          map = 'redo',
+          opts = {}
+        },
+        highlight_for_count = true,
+      })
+    end
+  },
+}
