d6561fe5c761ae481dfd92a46494171a86db3407
Patrick Müller <patrick@muepatrick.dev>
2024-06-17 12:10:55 +0200

    Add readermode for fixed cursor
    With C-z trigger a zenlike readermode that always keeps the cursor in
    the middle of the screen. Can be used when looking through files.

---
+++ lua/custom/plugins/readermode.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'sarrisv/readermode.nvim',
+    opts = {
+      enabled = false,
+      -- keymap = "<C-z>"
+    },
+    config = function()
+      vim.keymap.set("n", "<C-z>", ":ReaderMode<CR>", { desc = '[Z]en like readermode' })
+    end,
+  }
+}
