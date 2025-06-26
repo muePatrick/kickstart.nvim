df8ca19d2fae1166f76fef92a313d277d84b71c0
Patrick Müller <patrick@muepatrick.dev>
2025-06-26 14:36:24 +0200

    Update local highlight

--- lua/custom/plugins/local-highlight.lua
+++ lua/custom/plugins/local-highlight.lua
@@ -1,8 +1,17 @@
 return {
-  {
-      'tzachar/local-highlight.nvim',
-      config = function()
-        require('local-highlight').setup()
-      end
-  },
+  'tzachar/local-highlight.nvim',
+  dependencies = { 'folke/snacks.nvim' },
+  config = function()
+    require('local-highlight').setup({
+      insert_mode = false,
+      animate = {
+        enabled = true,
+        easing = 'inOutCubic',
+        duration = {
+          step = 10,   -- ms per step
+          total = 100, -- maximum duration
+        },
+      },
+    })
+  end
 }
