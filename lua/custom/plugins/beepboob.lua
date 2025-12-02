3a0932ad80efb641bc4edb28498116bbefb75e51
Patrick Müller <patrick@muepatrick.dev>
2025-12-02 18:46:15 +0100

    Add sound for completed tasky todo items

--- lua/custom/plugins/beepboob.lua
+++ lua/custom/plugins/beepboob.lua
@@ -5,8 +5,9 @@ return {
       audio_player = "ffplay",
       max_sounds = 20,
       sound_map = {
-        { auto_command = "BufWritePost",                               sound = "notification.mp3" },
-        { key_map = { mode = "n", key_chord = "q", blocking = false }, sound = "success.mp3" },
+        { auto_command = "BufWritePost",                                        sound = "notification.mp3" },
+        { key_map = { mode = "n", key_chord = "q", blocking = false },          sound = "success.mp3" },
+        { key_map = { mode = "n", key_chord = "<leader>xd", blocking = false }, sound = "chime.mp3" },
       }
     }
   }
