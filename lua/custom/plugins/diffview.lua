a1de4e983c659ecdb3b6e5c28f40ded0d17e97eb
Patrick Müller <patrick@muepatrick.dev>
2026-01-05 16:56:16 +0100

    Enable toggling for diffview pane

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -2,19 +2,31 @@ return {
   {
     'sindrets/diffview.nvim',
     config = function()
-      -- function IsDiffviewOpen()
-      --   for i in range(1, tabpagenr('$'))
-      --     let buflist = tabpagebuflist(i)
-      --     for bufnr in buflist
-      --       if bufname(bufnr) =~# '^diffview://'
-      --         return true
-      --       endif
-      --     endfor
-      --   endfor
-      --   return false
-      -- end
-      vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", { desc = '[G]it [D]iff View' })
-      vim.keymap.set("n", "<leader>gD", ":DiffviewClose<CR>", { desc = '[G]it [D]iff View Close' })
+      -- vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", { desc = '[G]it [D]iff View' })
+      -- vim.keymap.set("n", "<leader>gD", ":DiffviewClose<CR>", { desc = '[G]it [D]iff View Close' })
+
+      vim.keymap.set("n", "<leader>gd",
+        function()
+          local diffview_exists = false
+          for i = 1, vim.fn.tabpagenr('$') do
+            local buflist = vim.fn.tabpagebuflist(i)
+            for _, bufnr in ipairs(buflist) do
+              if vim.fn.bufname(bufnr):match('^diffview://') then
+                diffview_exists = true
+                break
+              end
+            end
+            if diffview_exists then break end
+          end
+          
+          if diffview_exists then
+            vim.cmd('DiffviewClose')
+          else
+            vim.cmd('DiffviewOpen')
+          end
+        end, 
+        { desc = '[G]it [D]iff View Toggle' }
+      )      
     end,
   }
 }
